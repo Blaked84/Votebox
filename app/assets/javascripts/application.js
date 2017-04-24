ce40b84a1520f759d5f68b8f5fee04627f91f5b6
blaked <dorian.becker@gadz.org>
2017-04-24 15:08:51 +0200

    :bug: fix action for not registered user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,10 +6,7 @@ $( document ).ready(function() {
     var projects = new Vue({
         el: '#projects',
         data: {
-            projects: [
-                { name: 'Learn Vue' },
-                { name: 'Build something awesome' }
-            ]
+            projects: []
         },
         mounted: function() {
             var that;
@@ -24,33 +21,39 @@ $( document ).ready(function() {
         methods: {
             vote: function (project, _for) {
                 var that = this;
-                $.ajax({
-                    method: 'POST',
-                    url: _for ? project.urls.vote_for : project.urls.vote_against,
-                    success: function(res) {
-                        $.ajax({
-                            url: '/projects.json',
-                            success: function(res) {
-                                that.projects = res;
-                            }
-                        });
-                    }
-                })
+                if (project.current_user.logged_user)
+                    $.ajax({
+                        method: 'POST',
+                        url: _for ? project.urls.vote_for : project.urls.vote_against,
+                        success: function(res) {
+                            $.ajax({
+                                url: '/projects.json',
+                                success: function(res) {
+                                    that.projects = res;
+                                }
+                            });
+                        }
+                    });
+                else
+                    location.href = "users/sign_in";
             },
             join: function (project, join) {
                 var that = this;
-                $.ajax({
-                    method: 'POST',
-                    url: join ? project.urls.join : project.urls.leave,
-                    success: function(res) {
-                        $.ajax({
-                            url: '/projects.json',
-                            success: function(res) {
-                                that.projects = res;
-                            }
-                        });
-                    }
-                })
+                if (project.current_user.logged_user)
+                    $.ajax({
+                        method: 'POST',
+                        url: join ? project.urls.join : project.urls.leave,
+                        success: function(res) {
+                            $.ajax({
+                                url: '/projects.json',
+                                success: function(res) {
+                                    that.projects = res;
+                                }
+                            });
+                        }
+                    });
+                else
+                    location.href = "users/sign_in";
             }
         }
 
