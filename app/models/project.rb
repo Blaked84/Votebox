68c435b0f62a43b187ce325f8a9e93647ab4532d
blaked <dorian.becker@gadz.org>
2017-04-24 20:18:42 +0200

    :lipstick: fix for missing avatar

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,7 +21,7 @@ class Project < ActiveRecord::Base
   def members_infos
     users.map{|m| [name: m.fullname,
                    avatar_url:  m.avatar_url,
-                   profile_url: "https://www.arts-et-metiers.asso.fr/index.php/annuaire2/profil/id/" + m.avatar_url.split("/").last.split("_").last.split(".").first
+                   # profile_url: "https://www.arts-et-metiers.asso.fr/index.php/annuaire2/profil/id/" + m.avatar_url.split("/").last.split("_").last.split(".").first
     ]}.flatten
   end
 
