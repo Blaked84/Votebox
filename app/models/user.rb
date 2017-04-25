f133a084754d5937c0fc939e787c74862f773110
blaked <dorian.becker@gadz.org>
2017-04-25 12:33:51 +0200

    :bug: fix rails autoreload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  require File.expand_path(GorgEngine::Engine.config.root + 'app' + 'models' + ( self.name.underscore + '.rb'))
+  require_dependency File.expand_path(GorgEngine::Engine.config.root + 'app' + 'models' + ( self.name.underscore + '.rb'))
+
 
   has_many :projects_members
   has_many :projects, through: :projects_members
